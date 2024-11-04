58088d37280c8e88600ff4d23ec745feb2d15a2c
KKKKHXX <119933864+kkkkhxx@users.noreply.github.com>
2024-11-04 05:20:57 +0000

    6687063_Lab14

--- calculator.py
+++ calculator.py
@@ -3,23 +3,37 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
-        return result
+        if b<0 and a<0:
+            b *= (-1)
+            a *= (-1)
+            for i in range(b):
+                result = self.add(result, a)
+            return result
+        elif b<a:
+            for i in range(a):
+                result = self.add(result, b)
+            return result
+        else:
+            for i in range(b):
+                result = self.add(result, a)
+            return result
 
     def divide(self, a, b):
-        result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
-        return result
+        result = 1
+        if a==0:
+            return 0
+        else:
+            while a > b:
+                a = self.subtract(a, b)
+                result += 1
+            return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,49 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+    def test_add_negative(self):
+        self.assertEqual(self.calc.add(-1, -2), -3)
+
+    def test_add_zero(self):
+        self.assertEqual(self.calc.add(1, 0), 1)
+    
+    def test__add_zero_negative(self):
+        self.assertEqual(self.calc.add(-3, 0), -3)
+
+    #test methods subtract:
+    def test_sub_negative(self):
+        self.assertEqual(self.calc.subtract(-1, -5), 4)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(-1, 3), -4)
+
+    #test methods multiply:
+    def test_mul(self):
+        self.assertEqual(self.calc.multiply(4, 8), 32)
+
+    def test_mul_zero(self):
+        self.assertEqual(self.calc.multiply(0, 8), 0)
+
+    def test_mul_negative(self):
+        self.assertEqual(self.calc.multiply(-2, -8), 16)
+
+    def test_mul_negative_num(self):
+        self.assertEqual(self.calc.multiply(2, -8), -16)
+    
+    #test methods divide:
+    def test_div_num(self):
+        self.assertEqual(self.calc.divide(9, 3), 3)
+
+    def test_div_negative(self):
+        self.assertEqual(self.calc.divide(0, 9), 0)
+
+    #test methods modulo:
+    def test_modulo_zero(self):
+        self.assertEqual(self.calc.modulo(10, 2), 0)
+    
+    def test_modulo_num(self):
+        self.assertEqual(self.calc.modulo(3, 7), 3)
+    
 
 if __name__ == '__main__':
     unittest.main()
